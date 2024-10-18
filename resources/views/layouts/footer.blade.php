f0277fc3f7a280d8b8570550eba0efed98cf56ef
Raihan <itsrahul880@gmail.com>
2024-10-19 00:37:29 +0600

    initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,28 @@
+</div>
+<div class="sidebar-overlay" data-reff=""></div>
+
+<script src="assets/js/jquery-3.7.1.min.js"></script>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/js/jquery.slimscroll.js"></script>
+
+<script src="assets/js/select2.min.js"></script>
+
+<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="assets/plugins/datatables/datatables.min.js"></script>
+
+<script src="assets/js/jquery.waypoints.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+
+<script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+<script src="assets/plugins/apexchart/chart-data.js"></script>
+
+<script src="assets/js/app.js"></script>
+{{-- <script src="../../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+    data-cf-settings="b76be323c2d9ff8fbb2037f0-|49" defer=""></script> --}}
+</body>
+
+</html>
